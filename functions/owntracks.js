a7643dd6a391fb74aa50a64520c579aded400e00
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-01-21 14:10:58 -0600

    Init Netlify Functions

---
+++ functions/owntracks.js
@@ -0,0 +1,23 @@
+const faunadb = require("faunadb");
+
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.FAUNADB_SECRET,
+});
+
+exports.handler = async (event, context) => {
+  try {
+    response = await client.query(
+      q.Create(q.Ref("classes/owntracks"), { data: JSON.parse(event.body) })
+    );
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify(error),
+    };
+  }
+};
