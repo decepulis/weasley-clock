3ab540e20de355e5b234526af21db255f40e2043
Darius Cepulis <dcepulis@microsensorlabs.com>
2021-01-21 16:03:22 -0600

    Improve function with basic auth and epoch parsing

--- functions/owntracks.js
+++ functions/owntracks.js
@@ -1,15 +1,28 @@
 const faunadb = require("faunadb");
 
-const q = faunadb.query;
+const { Create, Collection, Epoch } = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB_SECRET,
 });
 
 exports.handler = async (event, context) => {
+  data = JSON.parse(event.body);
+
+  if (event.headers.authorization !== process.env.API_PASSWORD) {
+    return {
+      statusCode: 403,
+    };
+  }
+
+  // We convert the provided timestamp
+  // into a FaunaDB Time() object
+  if (typeof data.tst === "number") {
+    data.tst = Epoch(data.tst, "seconds");
+  }
+
+  // Then, we post
   try {
-    response = await client.query(
-      q.Create(q.Ref("classes/owntracks"), { data: JSON.parse(event.body) })
-    );
+    response = await client.query(Create(Collection("owntracks"), { data }));
     return {
       statusCode: 200,
       body: JSON.stringify(response),
